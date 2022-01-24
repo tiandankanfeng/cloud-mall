f9354b16f9523b32292c55ca111f4af14ed7db11
夜痕 <zelin.wzl@alibaba-inc.com>
2022-01-24 21:33:13 +0800

    feat:简单一版实现数据库密码加密

--- cloud-mall-infrastructure/src/test/java/com/cloud/mall/infrastructure/CloudMallInfrastructureApplicationTests.java
+++ cloud-mall-infrastructure/src/test/java/com/cloud/mall/infrastructure/CloudMallInfrastructureApplicationTests.java
@@ -1,18 +1,36 @@
 package com.cloud.mall.infrastructure;
 
+import com.alibaba.druid.filter.config.ConfigTools;
+
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
 @Slf4j
 @RunWith(JUnit4.class)
 class CloudMallInfrastructureApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testDbOnEncrypt() throws Exception {
+        System.out.println("hello world");
+        val userName = "wu913428";
+        String[] keyPair = ConfigTools.genKeyPair(512);
+        // 私钥
+        val privateKey = keyPair[0];
+        // 公钥
+        val publicKey = keyPair[1];
+        // 加密
+        var encryptCode = ConfigTools.encrypt(privateKey, userName);
+        var decryptCode = ConfigTools.decrypt(publicKey, encryptCode);
+        System.out.println(StrUtil.format("私钥:{}, 加密后:{}", privateKey, encryptCode));
+        System.out.println(StrUtil.format("公钥:{}, 解密后:{}", publicKey, decryptCode));
     }
 
 }
