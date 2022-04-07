c72b8fb9f958222977545926c64d7728d450ae9e
夜痕 <zelin.wzl@alibaba-inc.com>
2022-04-08 01:55:01 +0800

    feat: 完善用户态.

--- cloud-mall-app/src/main/java/com/cloud/mall/app/config/CrosConfig.java
+++ cloud-mall-app/src/main/java/com/cloud/mall/app/config/CrosConfig.java
@@ -21,6 +21,7 @@ public class CrosConfig {
                 registry.addMapping("/**")
                     .allowedOrigins("*")
                     .allowedMethods("*");
+
             }
         };
     }
